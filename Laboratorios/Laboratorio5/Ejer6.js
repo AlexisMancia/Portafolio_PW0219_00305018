4007d64570585e098a79173ca9e35131121fafe2
Alexis Mancia <00305018@uca.edu.sv>
2019-09-25 11:03:17 -0600

    Fix ejer 6

--- Laboratorios/Laboratorio5/Ejer6.js
+++ Laboratorios/Laboratorio5/Ejer6.js
@@ -1,8 +1,16 @@
 
 /*Use sort no tenia tiempo */
-function ordenar(array){
-    array.sort(function(a, b){return a - b});
-    console.log(array);
+function ordenar(lista){
+    for(var i=0;i<ListeningStateChangedEvent.length;i++){
+        for(var j=0;j<ListeningStateChangedEvent.length-1;j++){
+            if(lista[j+i]<lista[j]){
+                var aux = lista[j+1];
+                lista[j+1] = lista[j];
+                lista[j] = aux;
+            }
+        }
+    }
+    return lista;
 }
 
 ordenar([2,5,6,774,21,0]);
